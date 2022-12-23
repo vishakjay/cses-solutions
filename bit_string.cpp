07a74320eb307f367753dfa37e7daaede96e9b1d
vishakjay <107393021+vishakjay@users.noreply.github.com>
2022-12-23 22:46:20 +0530

    Create bit_string.cpp

---
+++ bit_string.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+ 
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+ 
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+ 
+#define PI 3.1415926535897932384626433832795l 
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+ 
+// -------------------------<RNG>------------------------- 
+// RANDOM NUMBER GENERATOR
+mt19937 RNG(chrono::steady_clock::now().time_since_epoch().count());  
+#define SHUF(v) shuffle(all(v), RNG); 
+// Use mt19937_64 for 64 bit random numbers.
+bool checkSame(int a[],int n){
+   for(int i=1;i<=n-1;i++){
+      if(a[i-1]!=a[i]){
+         return true;
+      }
+   }
+   return false;
+}
+void solve(){
+ll n;
+cin>>n;
+ll M=1000000007;
+ll ans=1;
+for(ll i=0;i<n;i++){
+  ans=(ll)ans*2;
+  ans%=M;
+ }
+ cout<<ans<<"\n";
+}
+ 
+ 
+int main(){
+/*#ifndef ONLINE_JUDGE
+ 
+freopen("input.txt","r",stdin);
+ 
+freopen("output.txt","w",stdout);
+ 
+#endif*/
+ios_base::sync_with_stdio(0);
+cin.tie(0); cout.tie(0);
+ 
+int t=1;
+//cin>>t;
+ 
+ while(t--){
+  solve();
+ }
+ 
+return 0; 
+}
